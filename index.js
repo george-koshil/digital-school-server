48218055f29d5285ab698159a5d796b93e2291b0
george-koshil <66992395+george-koshil@users.noreply.github.com>
2023-03-23 14:58:39 +0200

    Add versel dependencies

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('../app');
+var app = require('./app');
 var debug = require('debug')('digital-school-server:server');
 var http = require('http');
 
